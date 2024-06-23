f574dceb22220c0f06c5f6288b74606b8b5b396b
oranuch.cha <oranuch.char@gmail.com>
2024-06-23 23:49:40 +0700

    initial project

---
+++ src/test/java/com/odt/restful_api_demo/RestfulApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odt.restful_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
